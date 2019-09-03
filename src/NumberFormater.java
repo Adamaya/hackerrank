0585e2e478136ed662cd2d49daff0f605d0e29f4
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-04 00:49:39 +0530

    Scanner object created

---
+++ src/NumberFormater.java
@@ -0,0 +1,13 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class NumberFormater {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double payment=input.nextDouble();
+        input.close();
+
+    }
+}
