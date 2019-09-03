b5f224c1ba0e51066e45626e03c27ac7a7622106
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-04 00:52:36 +0530

    print statement added with formatted variables(india.format(payment))

--- src/NumberFormater.java
+++ src/NumberFormater.java
@@ -13,5 +13,9 @@ public class NumberFormater {
         NumberFormat china= NumberFormat.getCurrencyInstance(Locale.CHINA);
         NumberFormat france= NumberFormat.getCurrencyInstance(Locale.FRANCE);
         NumberFormat us=NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("India "+india.format(payment));
+        System.out.println("China "+china.format(payment));
+        System.out.println("France "+france.format(payment));
+        System.out.println("US "+us.format(payment));
     }
 }
