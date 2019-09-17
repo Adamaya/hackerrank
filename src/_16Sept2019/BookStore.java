0c141747273c28751df9c8777df8a2b6fb175aba
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-17 22:34:40 +0530

    getter setter created of bookdetails

--- src/_16Sept2019/BookStore.java
+++ src/_16Sept2019/BookStore.java
@@ -4,6 +4,30 @@ class BookDetails{
     private String bookname;
     private String author;
     private double isbn;
+
+    public String getBookname(){
+        return bookname;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+
+    public double getIsbn(){
+        return isbn;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public void setIsbn(double isbn) {
+        this.isbn = isbn;
+    }
 }
 
 public class BookStore {
