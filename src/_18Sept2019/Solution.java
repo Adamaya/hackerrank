1827de054907534e368a639252ea16cdb3e05413
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-18 18:31:04 +0530

    variable declared

--- src/_18Sept2019/Solution.java
+++ src/_18Sept2019/Solution.java
@@ -1,7 +1,10 @@
 package _18Sept2019;
 
 class playerdetails {
-
+    private String name;
+    private String uname;
+    private String level;
+    private int highscore;
 }
 public class Solution {
 
