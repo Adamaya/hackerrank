d0d3663b30fa516f9a6f9a55ab71668ad5b90ba1
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-18 18:31:48 +0530

    getter setter created in playerdetail

--- src/_18Sept2019/Solution.java
+++ src/_18Sept2019/Solution.java
@@ -5,6 +5,39 @@ class playerdetails {
     private String uname;
     private String level;
     private int highscore;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public int getHighscore() {
+        return highscore;
+    }
+
+    public void setHighscore(int highscore) {
+        this.highscore = highscore;
+
+    }
 }
 public class Solution {
 
