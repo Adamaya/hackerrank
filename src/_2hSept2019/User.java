96533936e3cb0429d02f4173a492d79c8f634633
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-25 18:00:39 +0530

    complete code

--- src/_2hSept2019/User.java
+++ src/_2hSept2019/User.java
@@ -12,10 +12,61 @@ public class User {
         this.companyNmae = companyNmae;
         this.dateOfJoining = dateOfJoining;
         this.uEmail = createUserEmail();
+        this.uPassword = createuPassword();
+
+
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Adamaya", "Acon", 2018);
+        System.out.println(user1.createuPassword());
+        System.out.println(user1.createUserEmail());
+    }
+
+    private String createuPassword() {
+        StringBuilder cp = new StringBuilder();
+        cp.append(generateRandomCharacter());
+        for (int i = 0; i < 4; i++) {
+            cp.append(generateRandomNumber());
+        }
+        cp.append(generateRandomString().toLowerCase());
+        cp.append(generateRandomSpecialSymbol());
+        return cp.toString();
+
+    }
+
+    private int generateRandomIntegerFromLimit(int limit) {
+        return (int) (Math.random() * limit);
+    }
+
+    private char generateRandomCharacter() {
+        return (char) ('a' + generateRandomIntegerFromLimit(26));
+    }
+
+    private int generateRandomNumber() {
+        return generateRandomIntegerFromLimit(10);
+    }
+
+    private String generateRandomString() {
+        if (getuName().length() < 5) {
+            StringBuilder pass = new StringBuilder();
+            int defference = 5 - getuName().length();
+            pass.append("0".repeat(Math.max(0, defference)));
+            return pass.toString();
+
+        }
+        return getuName().substring(0, 5);
+    }
+
+    private Character generateRandomSpecialSymbol() {
+        char[] specialSymbol = {
+                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', ';', '<', '>', '?', '\\'
+        };
+        return specialSymbol[generateRandomIntegerFromLimit(specialSymbol.length)];
     }
 
     private String createUserEmail() {
-        return String.format("%s_%d@%s", getuName(), getDateOfJoining(), getCompanyNmae());
+        return String.format("%s_%d@%s.com", getuName().toLowerCase(), getDateOfJoining(), getCompanyNmae().toLowerCase());
     }
 
     public String getuName() {
