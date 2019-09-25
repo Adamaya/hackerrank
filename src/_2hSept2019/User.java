34b13bb601d77c014b5e0d2579d83883d3548f3a
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-09-25 16:33:22 +0530

    getter setter created

--- src/_2hSept2019/User.java
+++ src/_2hSept2019/User.java
@@ -5,5 +5,27 @@ public class User {
     private String companyNmae;
     private String dateOfJoining;
 
+    public String getuName() {
+        return uName;
+    }
 
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+
+    public String getCompanyNmae() {
+        return companyNmae;
+    }
+
+    public void setCompanyNmae(String companyNmae) {
+        this.companyNmae = companyNmae;
+    }
+
+    public String getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(String dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
 }
