6782f5c3e57e0497bf2b18d867fda408bfc87e31
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 18:02:23 +0530

    character array created

--- src/ques/stringBufferBuilder/Ques8.java
+++ src/ques/stringBufferBuilder/Ques8.java
@@ -6,6 +6,6 @@ public class Ques8 {
     public static void main(String[] args) {
         int sum = 0;
         Scanner input = new Scanner(System.in);
-
+        char[] inputCharArray = input.nextLine().toCharArray();
     }
 }
