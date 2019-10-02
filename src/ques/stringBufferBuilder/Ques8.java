4106f6afe297dd510d38c0c003483011e4d66316
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 18:00:44 +0530

    Scanner object created

--- src/ques/stringBufferBuilder/Ques8.java
+++ src/ques/stringBufferBuilder/Ques8.java
@@ -1,7 +1,9 @@
 package ques.stringBufferBuilder;
 
+import java.util.Scanner;
+
 public class Ques8 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
     }
 }
