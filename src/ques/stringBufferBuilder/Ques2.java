2c26d0a003f37d3464bf8fb83bf6abb0f6da92b8
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 16:39:36 +0530

    while loop created for rerun

--- src/ques/stringBufferBuilder/Ques2.java
+++ src/ques/stringBufferBuilder/Ques2.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 public class Ques2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        try {
-            String inputString = input.nextLine();
-            int subStringRange = input.nextInt();
-            System.out.println(inputString.substring(0, subStringRange));
-        } catch (NoSuchElementException e) {
-            System.out.println("please enter the valid entry");
+        Boolean flag = true;
+        while (flag = true) {
+            try {
+                String inputString = input.nextLine();
+                int subStringRange = input.nextInt();
+                System.out.println(inputString.substring(0, subStringRange));
+            } catch (NoSuchElementException e) {
+                System.out.println("please enter the valid entry");
+            }
         }
 
 
