46b2404aec0b60a9e6d48ae9dab7c1815c2b6a0f
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 18:32:47 +0530

    sumUpper =0 & sumLower = 0 initialized

--- src/ques/stringBufferBuilder/Ques9.java
+++ src/ques/stringBufferBuilder/Ques9.java
@@ -1,7 +1,19 @@
 package ques.stringBufferBuilder;
 
+import java.util.Scanner;
+
 public class Ques9 {
     public static void main(String[] args) {
+        int sumUpper = 0;
+        int sumLower = 0;
+        Scanner input = new Scanner(System.in);
+        char[] inputCharArray = input.nextLine().toCharArray();
+        for (int i = 0; i < inputCharArray.length; i++) {
+            int val = inputCharArray[i];
+            if (val >= 65 && val <= 90) {
+                sumUpper += val;
+            }
 
+        }
     }
 }
