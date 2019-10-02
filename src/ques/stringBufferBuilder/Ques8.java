769b7b2b6e24d036cba84df37465096c64f5a940
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 18:08:42 +0530

    if condition (val) logic added

--- src/ques/stringBufferBuilder/Ques8.java
+++ src/ques/stringBufferBuilder/Ques8.java
@@ -10,7 +10,7 @@ public class Ques8 {
         for (int i = 0; i < inputCharArray.length; i++) {
             int val = inputCharArray[i];
             if (val >= 65 && val <= 90) {
-
+                sum += val;
             }
         }
     }
