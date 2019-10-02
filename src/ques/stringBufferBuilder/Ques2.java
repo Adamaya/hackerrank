641db8f297aff8a64f28a28c385a18b7b0c22cda
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 16:37:19 +0530

    exceptional handling added

--- src/ques/stringBufferBuilder/Ques2.java
+++ src/ques/stringBufferBuilder/Ques2.java
@@ -1,12 +1,19 @@
 package ques.stringBufferBuilder;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Ques2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String inputString = input.nextLine();
-        int subStringRange = input.nextInt();
-        System.out.println(inputString.substring(0, subStringRange));
+        try {
+            String inputString = input.nextLine();
+            int subStringRange = input.nextInt();
+            System.out.println(inputString.substring(0, subStringRange));
+        } catch (NoSuchElementException e) {
+            System.out.println("please enter the valid entry");
+        }
+
+
     }
 }
