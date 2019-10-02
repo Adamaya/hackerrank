2e91fa43cb7a4365a3de5bb87a73068341bf6343
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 17:57:08 +0530

    int sum=0 initialized

--- src/ques/stringBufferBuilder/Ques7.java
+++ src/ques/stringBufferBuilder/Ques7.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Ques7 {
     public static void main(String[] args) {
+        int sum = 0;
         Scanner input = new Scanner(System.in);
         char[] inputCharArray = input.nextLine().toCharArray();
         for (int i = 0; i < inputCharArray.length; i++) {
             int val = inputCharArray[i];
+
 
         }
     }
