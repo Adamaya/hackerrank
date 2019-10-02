c8f83eb8c8b91350c2120ed65bd97dae959d27ea
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 16:06:27 +0530

    "index:\t" printed

--- src/ques/stringBufferBuilder/Ques1.java
+++ src/ques/stringBufferBuilder/Ques1.java
@@ -6,5 +6,7 @@ public class Ques1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] arrayInputString = input.nextLine().split("");
+        System.out.print("index:\t");
+
     }
 }
