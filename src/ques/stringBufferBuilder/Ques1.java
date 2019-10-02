b1f80afe299650e51f3e05c0efb4304c428a6069
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 16:05:38 +0530

    string type input array created

--- src/ques/stringBufferBuilder/Ques1.java
+++ src/ques/stringBufferBuilder/Ques1.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Ques1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        String[] arrayInputString = input.nextLine().split("");
     }
 }
