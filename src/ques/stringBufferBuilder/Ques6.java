3a9eb21ce7ee1e819fa7951005b2d1aabe965220
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 17:40:56 +0530

    printing inputString in uppercase

--- src/ques/stringBufferBuilder/Ques6.java
+++ src/ques/stringBufferBuilder/Ques6.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Ques6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        System.out.println(input.nextLine().toUpperCase());
     }
 }
