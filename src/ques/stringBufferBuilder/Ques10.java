12d7c6a6771cb5ed26e217115742a8e9dfadcbce
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 22:54:40 +0530

    inputString initialized

--- src/ques/stringBufferBuilder/Ques10.java
+++ src/ques/stringBufferBuilder/Ques10.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Ques10 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
 
     }
 }
