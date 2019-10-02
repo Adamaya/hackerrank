79993129b720d124998f5e8d8b935e79cd0c28ce
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 16:59:03 +0530

    StringOutOfBoundException handling added

--- src/ques/stringBufferBuilder/Ques2.java
+++ src/ques/stringBufferBuilder/Ques2.java
@@ -7,13 +7,16 @@ public class Ques2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Boolean flag = true;
-        while (flag = true) {
+        while (flag) {
             try {
-                String inputString = input.nextLine();
+                String inputString = input.nextLine().trim();
                 int subStringRange = input.nextInt();
                 System.out.println(inputString.substring(0, subStringRange));
+                flag = false;
             } catch (NoSuchElementException e) {
                 System.out.println("please enter the valid entry");
+            } catch (StringIndexOutOfBoundsException e) {//StringIndexOutOfBoundsException
+                System.out.println("enter valid range");
             }
         }
 
