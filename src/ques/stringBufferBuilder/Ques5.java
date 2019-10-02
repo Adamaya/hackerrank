a6e0986d53c5b62431cd3f6231f633b3bd3a5e18
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-02 17:35:55 +0530

    string replaced

--- src/ques/stringBufferBuilder/Ques5.java
+++ src/ques/stringBufferBuilder/Ques5.java
@@ -3,6 +3,6 @@ package ques.stringBufferBuilder;
 public class Ques5 {
     public static void main(String[] args) {
         String string = "Elon Zuckerburg";
-
+        string = string.replaceAll("Zuckerburg", "Musk");
     }
 }
