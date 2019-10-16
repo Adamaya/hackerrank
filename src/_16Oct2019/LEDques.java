cae8db6eb44e56283229f4570934c850674a0056
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-16 16:55:36 +0530

    signature logic added in LEDString class

--- src/_16Oct2019/LEDques.java
+++ src/_16Oct2019/LEDques.java
@@ -9,6 +9,10 @@ class LED {
         this.working = working;
     }
 
+    public LED(boolean working) {
+        this.working = working;
+    }
+
     public String getColor() {
         return color;
     }
@@ -27,17 +31,42 @@ class LED {
 }
 
 class LEDStrip {
-    private int lengthOfStrip() {
+    private LED[] listOfLed;
 
+    public LEDStrip(LED[] listOfLed) {
+        this.listOfLed = listOfLed;
+    }
+
+    private int lengthOfStrip() {
+        return listOfLed.length;
+    }
+
+    public LED[] getListOfLed() {
+        return listOfLed;
+    }
+
+    public void setListOfLed(LED[] listOfLed) {
+        this.listOfLed = listOfLed;
     }
 
     private int numberOfDefectiveLEDs() {
-
+        int workingLed = 0;
+        for (int i = 0; i < lengthOfStrip(); i++) {
+            if (!listOfLed[i].isWorking()) {
+                workingLed++;
+            }
+        }
+        return workingLed;
     }
 
     private boolean isDefective() {
+        return numberOfDefectiveLEDs() > lengthOfStrip() / 2;
+    }
 
+    public String stripStatus() {
+        return isDefective() ? "This strip is defective." : "This Strip is not defective";
     }
 }
 public class LEDques {
+
 }
