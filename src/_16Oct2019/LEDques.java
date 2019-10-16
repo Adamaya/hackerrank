5a2143857fa988bd27236d22d75a926817f0b00e
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-16 16:22:55 +0530

    constructor and getter setters created

--- src/_16Oct2019/LEDques.java
+++ src/_16Oct2019/LEDques.java
@@ -1,6 +1,29 @@
 package _16Oct2019;
 
 class LED {
+    private String color;
+    private boolean working;
+
+    public LED(String color, boolean working) {
+        this.color = color;
+        this.working = working;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isWorking() {
+        return working;
+    }
+
+    public void setWorking(boolean working) {
+        this.working = working;
+    }
 
 }
 public class LEDques {
