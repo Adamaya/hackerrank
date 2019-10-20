651f9301efcfbfef49d508a2244a8ab3406adb79
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 21:37:54 +0530

    numbercounter class created

--- src/practicequestion/carcomparator/CarComparator.java
+++ src/practicequestion/carcomparator/CarComparator.java
@@ -1,4 +1,7 @@
 package practicequestion.carcomparator;
 
 public class CarComparator {
+    private double costOfCNG = 50;
+    private double costofPetrol = 80;
+
 }
---
+++ src/practicequestion/numbercounter/Numbercounter.java
@@ -0,0 +1,4 @@
+package practicequestion.numbercounter;
+
+public class Numbercounter {
+}
