a6fc79c59540ee5e66388decbc3208cc75ce77ba
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 21:58:35 +0530

    feild mileage and its getter setter created

--- src/practicequestion/premmandir/PremMandir.java
+++ src/practicequestion/premmandir/PremMandir.java
@@ -1,4 +1,17 @@
 package practicequestion.premmandir;
 
 public class PremMandir {
+    private double mileage;
+
+    public PremMandir(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
 }
