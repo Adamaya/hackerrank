bc0e9f87df40c6460ff55106fe0fa8437cc9a86b
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 14:35:25 +0530

    create: object: input(Scanner class)

--- src/practicequestion/guessnumber/GuessNumber.java
+++ src/practicequestion/guessnumber/GuessNumber.java
@@ -1,9 +1,11 @@
 package practicequestion.guessnumber;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Random random = new Random();
         int number = random.nextInt(100) + 1;
 
