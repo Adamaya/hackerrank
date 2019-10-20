bcde241944942519b90e9aa377ff41179958b5f7
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 12:44:25 +0530

    getter setter created

--- src/practicequestion/tempratureconvertor/TempratureConvertor.java
+++ src/practicequestion/tempratureconvertor/TempratureConvertor.java
@@ -3,4 +3,11 @@ package practicequestion.tempratureconvertor;
 public class TempratureConvertor {
     private double celsius;
 
+    public double getCelsius() {
+        return celsius;
+    }
+
+    public void setCelsius(double celsius) {
+        this.celsius = celsius;
+    }
 }
