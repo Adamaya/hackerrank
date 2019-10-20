502663a0d34c61d9496ef7a9207647f465f36f75
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 13:19:38 +0530

    create: getter setter: seconds

--- src/practicequestion/timeconvertor/TimeConvertor.java
+++ src/practicequestion/timeconvertor/TimeConvertor.java
@@ -3,5 +3,11 @@ package practicequestion.timeconvertor;
 public class TimeConvertor {
     private double seconds;
 
+    public double getSeconds() {
+        return seconds;
+    }
 
+    public void setSeconds(double seconds) {
+        this.seconds = seconds;
+    }
 }
