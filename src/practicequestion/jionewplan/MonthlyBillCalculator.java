5987ca2a9e343b7c15ce08261580cfc0f6e9643a
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 18:18:55 +0530

    create: getter setter: numberOfMinutes

--- src/practicequestion/jionewplan/MonthlyBillCalculator.java
+++ src/practicequestion/jionewplan/MonthlyBillCalculator.java
@@ -3,4 +3,11 @@ package practicequestion.jionewplan;
 public class MonthlyBillCalculator {
     private int numberOfMinutes;
 
+    public int getNumberOfMinutes() {
+        return numberOfMinutes;
+    }
+
+    public void setNumberOfMinutes(int numberOfMinutes) {
+        this.numberOfMinutes = numberOfMinutes;
+    }
 }
