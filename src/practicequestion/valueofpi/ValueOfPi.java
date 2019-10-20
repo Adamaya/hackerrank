5ae5eb550568255beecdad600d96309890c02c67
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-21 00:03:32 +0530

    input variable number created

--- src/practicequestion/valueofpi/ValueOfPi.java
+++ src/practicequestion/valueofpi/ValueOfPi.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class ValueOfPi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
     }
 }
