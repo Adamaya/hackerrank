b81fba36c398e61ab14958f2d52701369d2385f0
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 14:45:45 +0530

    create: if condition: number==yourNumber

--- src/practicequestion/guessnumber/GuessNumber.java
+++ src/practicequestion/guessnumber/GuessNumber.java
@@ -7,8 +7,12 @@ public class GuessNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int yourNumber = input.nextInt();
+        input.close();
         Random random = new Random();
         int number = random.nextInt(100) + 1;
+        if (number == yourNumber) {
+            System.out.println("Yes, you are right, I was thinking of " + number + "!");
+        }
 
     }
 }
