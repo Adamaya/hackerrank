8720a1edcd5824be5343edf26a7f893ac888ab12
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 12:50:44 +0530

    celsius field and its getter setters added

--- src/practicequestion/tempratureconvertor/TempratureConvertor.java
+++ src/practicequestion/tempratureconvertor/TempratureConvertor.java
@@ -1,8 +1,17 @@
 package practicequestion.tempratureconvertor;
 
 public class TempratureConvertor {
+    private double celsius;
+
+    public double getCelsius() {
+        return celsius;
+    }
+
+    public void setCelsius(double celsius) {
+        this.celsius = celsius;
+    }
+
     public double celsiusToFahrenheit(double tempInCelsius) {
 
     }
-
 }
