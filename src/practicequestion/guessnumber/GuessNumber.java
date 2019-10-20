744cb52628feb7aa79c40296012931ba6ec4711b
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 14:34:20 +0530

    create: field: number: initialized with random number

--- src/practicequestion/guessnumber/GuessNumber.java
+++ src/practicequestion/guessnumber/GuessNumber.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class GuessNumber {
     public static void main(String[] args) {
         Random random = new Random();
+        int number = random.nextInt(100) + 1;
 
     }
 }
