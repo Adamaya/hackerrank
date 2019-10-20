41e4e1d5a33ad8f13b5e7b2dd33a11afa7642d2d
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 12:47:27 +0530

    celsius field and his getter setter deleted

--- src/practicequestion/tempratureconvertor/TempratureConvertor.java
+++ src/practicequestion/tempratureconvertor/TempratureConvertor.java
@@ -1,13 +1,6 @@
 package practicequestion.tempratureconvertor;
 
 public class TempratureConvertor {
-    private double celsius;
 
-    public double getCelsius() {
-        return celsius;
-    }
 
-    public void setCelsius(double celsius) {
-        this.celsius = celsius;
-    }
 }
