0b22da77546b3ef7fe4e81196e99e6b3655d768f
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 21:38:47 +0530

    Scanner instance created

--- src/practicequestion/numbercounter/Numbercounter.java
+++ src/practicequestion/numbercounter/Numbercounter.java
@@ -1,7 +1,9 @@
 package practicequestion.numbercounter;
 
+import java.util.Scanner;
+
 public class Numbercounter {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
     }
 }
