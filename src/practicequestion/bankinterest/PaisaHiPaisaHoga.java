5e5fc458e21bb20612a04a350b533effc82e9c90
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 14:51:33 +0530

    create: getters setters: principalMoney,rate

--- src/practicequestion/bankinterest/PaisaHiPaisaHoga.java
+++ src/practicequestion/bankinterest/PaisaHiPaisaHoga.java
@@ -3,4 +3,20 @@ package practicequestion.bankinterest;
 public class PaisaHiPaisaHoga {
     private double principalMoney;
     private double rate;
+
+    public double getPrincipalMoney() {
+        return principalMoney;
+    }
+
+    public void setPrincipalMoney(double principalMoney) {
+        this.principalMoney = principalMoney;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
 }
