ff34f2d5dfc1af6bd036bb06edeb7745a2137c9f
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 14:36:43 +0530

    create: input field: yourNumber

--- src/practicequestion/guessnumber/GuessNumber.java
+++ src/practicequestion/guessnumber/GuessNumber.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int yourNumber = input.nextInt();
         Random random = new Random();
         int number = random.nextInt(100) + 1;
 
