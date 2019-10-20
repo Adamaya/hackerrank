fab0d582e77273ff5307be5ee75df6778b495de4
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 21:39:23 +0530

    input number variable created

--- src/practicequestion/numbercounter/Numbercounter.java
+++ src/practicequestion/numbercounter/Numbercounter.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class Numbercounter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
     }
 }
