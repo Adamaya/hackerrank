cc50fc6136f8445657edef08a796974343443786
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 18:39:33 +0530

    delete: if else condition

--- src/practicequestion/jionewplan/MonthlyBillCalculator.java
+++ src/practicequestion/jionewplan/MonthlyBillCalculator.java
@@ -16,14 +16,6 @@ public class MonthlyBillCalculator {
     }
 
     public double MonthlyBill(int numberOfMinutes) {
-        double bill;
-        if (numberOfMinutes <= 300) {
-            bill = 0;
-        } else if (numberOfMinutes > 300) {
-            double callAmount = (300 - numberOfMinutes) * 1.2;
-            double gstAmount = (callAmount * 18) / 100;
-            bill = callAmount + gstAmount;
-        }
 
     }
 }
