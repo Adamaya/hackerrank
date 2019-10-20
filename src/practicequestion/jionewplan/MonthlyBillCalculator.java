f6bfd39c1910149e497d8ed93186795c29b7900d
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-20 18:31:56 +0530

    condition: else if: numberOfMinutes>300(logic updated)

--- src/practicequestion/jionewplan/MonthlyBillCalculator.java
+++ src/practicequestion/jionewplan/MonthlyBillCalculator.java
@@ -16,10 +16,14 @@ public class MonthlyBillCalculator {
     }
 
     public double MonthlyBill(int numberOfMinutes) {
+        double bill;
         if (numberOfMinutes <= 300) {
-            double bill = 0;
+            bill = 0;
         } else if (numberOfMinutes > 300) {
-
+            double callAmount = (300 - numberOfMinutes) * 1.2;
+            double gstAmount = (callAmount * 18) / 100;
+            bill = callAmount + gstAmount;
         }
+
     }
 }
