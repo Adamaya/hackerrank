1a6685953771c97d835ccfbcb8f63af4238f65f7
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-21 23:03:17 +0530

    if else condition added

--- src/practicequestion/mycalculator/MyCalculator.java
+++ src/practicequestion/mycalculator/MyCalculator.java
@@ -3,10 +3,17 @@ package practicequestion.mycalculator;
 import java.util.Scanner;
 
 public class MyCalculator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NegativeCustomException, ZeroCustomException {
         Scanner input = new Scanner(System.in);
         double n = input.nextDouble();
         double p = input.nextDouble();
-        System.out.println(Math.pow(n, p));
+        if (n < 0) {
+            throw new NegativeCustomException(n);
+        } else if (p < 0) {
+            throw new NegativeCustomException(p);
+        } else if (n == 0 || p == 0) {
+            throw new ZeroCustomException();
+        }
+
     }
 }
