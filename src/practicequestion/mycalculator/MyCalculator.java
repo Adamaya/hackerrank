02c82f503b2055afd7ea3a8d4a0204a74ef07afe
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-21 22:32:38 +0530

    Scanner class created

--- src/practicequestion/mycalculator/MyCalculator.java
+++ src/practicequestion/mycalculator/MyCalculator.java
@@ -1,7 +1,9 @@
 package practicequestion.mycalculator;
 
+import java.util.Scanner;
+
 public class MyCalculator {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
     }
 }
