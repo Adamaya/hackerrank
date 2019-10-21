da3511d079e575ece518ed544cc10efa25d96fcf
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-10-21 23:00:56 +0530

    NegativeCustomException class toString method override

--- src/practicequestion/mycalculator/NegativeCustomException.java
+++ src/practicequestion/mycalculator/NegativeCustomException.java
@@ -1,4 +1,13 @@
 package practicequestion.mycalculator;
 
 public class NegativeCustomException extends Exception {
+    private double numberDetails;
+
+    NegativeCustomException(double number) {
+        numberDetails = number;
+    }
+
+    public String toString() {
+        return numberDetails + " Negative number not Allowed";
+    }
 }
